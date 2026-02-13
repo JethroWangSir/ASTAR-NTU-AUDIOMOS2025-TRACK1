76e1503c3cce7d917405fa749672e1a6cff4806d
jethrowangsir <jethrowang0531@gmail.com>
2026-02-13 22:00:14 +0800

    Listwise ranking loss

--- run_best_single_model.sh
+++ run_best_single_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 
 python train.py \
     --expname primary_model_gaussian_listwise_ranking_loss \
@@ -9,12 +9,13 @@ python train.py \
     --train_list_path /share/nas169/jethrowang/MusicEval-full/sets/train_mos_list.txt \
     --validation_list_path /share/nas169/jethrowang/MusicEval-full/sets/dev_mos_list.txt \
     --test_list_path /share/nas169/jethrowang/MusicEval-full/sets/test_mos_list.txt \
-    --batch_size 30 \
-    --valid_batch_size 30 \
+    --batch_size 32 \
+    --valid_batch_size 32 \
     --lr 5e-5 \
     --optimizer adamw \
     --dist_prediction_score_style gaussian \
     --num_bins 20 \
     --use_ranking_loss \
     --ranking_loss_type listwise \
+    --rank_lambda 0.2 \
     --listwise_temperature 1.0

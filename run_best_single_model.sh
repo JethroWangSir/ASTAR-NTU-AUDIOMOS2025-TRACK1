8f167b4fcb8f3202dbdf30f4455b5d5062d6a398
jethrowangsir <jethrowang0531@gmail.com>
2026-02-18 23:35:32 +0800

    Adaptive beta

--- run_best_single_model.sh
+++ run_best_single_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python train.py \
     --expname primary_model_beta \
@@ -9,8 +9,8 @@ python train.py \
     --train_list_path /share/nas169/jethrowang/MusicEval-full/person_mos/train_person_mos.txt \
     --validation_list_path /share/nas169/jethrowang/MusicEval-full/person_mos/dev_person_mos.txt \
     --test_list_path /share/nas169/jethrowang/MusicEval-full/person_mos/test_person_mos.txt \
-    --batch_size 28 \
-    --valid_batch_size 28 \
+    --batch_size 32 \
+    --valid_batch_size 32 \
     --lr 5e-5 \
     --optimizer adamw \
     --dist_prediction_score_style beta \
